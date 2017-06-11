887b567f710085056b025e53c7278565d3e48694
heoDolf <dnjsakf@gmail.com>
2017-06-11 17:06:45 +0900

    webpage construction

--- app.js
+++ app.js
@@ -1,20 +1,34 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var route = express.Router();
 var app = express();
 var server = require('http').createServer(app);
 
+app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(bodyParser.urlencoded({extended:false}));
+app.use(express.static('public'));
 
-app.set(express.static('public'));
-
+/*********/
+/* Route */
+/*********/
 app.get('/main', function(req, res){
-  res.send('This is robby');
+  res.render('main');
+});
+app.get('/robby/:robbyNumber', function(req, res){
+  var robbyNumber = req.params.robbyNumber;
+  res.render('robby' , {robbyNum:robbyNumber});
+});
+app.get('/room/:roomNumber/:roomName', function(req, res){
+  var roomNumber = req.params.roomNumber;
+  var roomName = req.params.roomName;
+  res.render('room', {roomName:roomName, roomNumber:roomNumber});
 });
 
-app.get('/robby', function(req, res){
-  res.send('test');
-});
+/**********/
+/* socket */
+/**********/
 
 server.listen('3000', function(req, res){
   console.log('Connected Server 3000 port!!');
